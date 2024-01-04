5c00a3095adac5e4aaee5af0c3c4f40928f266bd
Poh Yan <wongpy-am19@student.tarc.edu.my>
2024-01-04 14:49:49 +0800

    training drift 13

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,27 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:training_drift/data/local/db/app_db.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AppDb _db;
+
+  @override
+  void initState() {
+    super.initState();
+    _db = AppDb();
+  }
+
+  @override
+  void dispose() {
+    _db.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        backgroundColor: Colors.blue[200],
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: FutureBuilder<List<EmployeeData>>(
+        future: _db.getEmployees(),
+        builder: (context, snapshot) {
+          final List<EmployeeData>? employees = snapshot.data;
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (employees != null) {
+            return ListView.builder(
+              itemCount: employees.length,
+              itemBuilder: (context, index) {
+                final employee = employees[index];
+                return Card(
+                  child: Column(
+                    children: [
+                      Text(employee.id.toString()),
+                      Text(employee.userName.toString()),
+                      Text(employee.firstName.toString()),
+                      Text(employee.lastName.toString()),
+                      Text(employee.dateOfBirth.toString()),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: Text("No data available"),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.pushNamed(
-          context,
-          "/add_employee",
+            context,
+            "/add_employee",
           );
         },
         icon: const Icon(Icons.add),
-        label: Text("Add Employee"),
+        label: const Text("Add Employee"),
       ),
     );
   }
