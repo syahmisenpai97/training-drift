8a314ce38f66d5b4275e0e1027b38f6fd0278b3d
Poh Yan <wongpy-am19@student.tarc.edu.my>
2024-01-04 12:00:10 +0800

    training drift 8

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:training_drift/screen/home_screen.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Demo',
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
